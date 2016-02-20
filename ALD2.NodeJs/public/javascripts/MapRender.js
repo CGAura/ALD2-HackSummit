4f518f642dd6ffa1a3f0193d9c3151b906c13de7
Alistair McKellar <ali.mckellar@outlook.com>
2016-02-20 19:10:05 +0000

    Moved MapRendering to seperate .js

---
+++ ALD2.NodeJs/public/javascripts/MapRender.js
@@ -0,0 +1,24 @@
+﻿var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('MyGoogleMapID'), {
+        zoom: 11,
+        center: { lat: 51.50, lng: -0.127 }
+    });
+    map.data.loadGeoJson('/KlmData/');
+    
+    map.data.setStyle(function (feature) {
+        var color = feature.getProperty('color');
+        return {
+            fillColor: color,
+            strokeWeight: 0.5
+        };
+    });
+    
+    map.data.addListener('click', function (event) {
+        document.getElementById('MyInfo').textContent =
+                  event.feature.getProperty('ccgCode');
+    });
+    
+    return true;
+}
